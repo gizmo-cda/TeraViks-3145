dca30c3dd214fb081ab5b27e688ac76c63876406
Crista Falk <Crista2019@users.noreply.github.com>
2018-01-30 15:17:25 -0800

    Create vision.py

---
+++ FirstPowerUp/vision.py
@@ -0,0 +1,37 @@
+from cscore import CameraServer
+
+import cv2
+import numpy as np
+
+def main():
+	cs = CameraServer.getInstance()
+	cs.enableLogging()
+
+	camera = cs.startAutomaticCapture()
+
+	camera.setResolution(320, 240)
+
+	# Get a CvSink. This will capture images from the camera
+	cvSink = cs.getVideo()
+
+	# (optional) Setup a CvSource. This will send images back to the Dashboard
+	outputStream = cs.putVideo("Rectangle", 320, 240)
+
+	# Allocating new images is very expensive, always try to preallocate
+	img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)    
+
+	while True:
+		# Tell the CvSink to grab a frame from the camera and put it
+		# in the source image.  If there is an error notify the output.
+		time, img = cvSink.grabFrame(img)
+		if time == 0:
+			# Send the output the error.
+			outputStream.notifyError(cvSink.getError());
+			# skip the rest of the current iteration
+			continue
+
+		# Put a rectangle on the image
+		cv2.rectangle(img, (110, 70), (210, 170), (0, 255, 0), 5)
+
+		# Give the output stream a new image to display
+		outputStream.putFrame(img)
