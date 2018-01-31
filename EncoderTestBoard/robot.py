9e6569fe76878102d41cf5f6275ea86cd1e4004a
Crista Falk <Crista2019@users.noreply.github.com>
2018-01-30 17:30:41 -0800

    Update robot.py
    Encoder limits with joystick

--- EncoderTestBoard/robot.py
+++ EncoderTestBoard/robot.py
@@ -4,7 +4,6 @@ import wpilib.buttons
 
 class Robot(wpilib.IterativeRobot):
   	
-
 	def robotInit(self):
 
 		self.talon = ctre.wpi_talonsrx.WPI_TalonSRX(0)
@@ -12,22 +11,37 @@ class Robot(wpilib.IterativeRobot):
 		self.button =  wpilib.buttons.JoystickButton(self.joystick, 3)
 		self.button2 = wpilib.buttons.JoystickButton(self.joystick, 2)
 
-
 	def teleopPeriodic(self):
+		isOn = .05
 		while self.isOperatorControl and self.isEnabled:
-			if self.button.get():
-				if self.talon.getQuadraturePosition() <= 20:
-					self.talon.set(.05)
+
+			if self.joystick.getY() > 0.1:
+				if self.talon.getQuadraturePosition() <= 1025:
+					self.talon.set(.08)
 					print(self.talon.getQuadraturePosition())
 				else:
 					self.talon.set(0)
-			elif self.button2.get():
-				if self.talon.getQuadraturePosition() >= 0:
-					self.talon.set(-.05)
+			elif self.joystick.getY() < -0.1:
+				if self.talon.getQuadraturePosition() >= 0: 
+					self.talon.set(-.08)
 					print(self.talon.getQuadraturePosition())
 				else:
 					self.talon.set(0)
-			
+			else:
+				self.talon.set(0)
+
+			# if self.button.get():
+			# 	if self.talon.getQuadraturePosition() <= 1025:
+			# 		self.talon.set(.05)
+			# 		print(self.talon.getQuadraturePosition())
+			# 	else:
+			# 		self.talon.set(0)
+			# elif self.button2.get():
+			# 	if self.talon.getQuadraturePosition() >= 0:
+			# 		self.talon.set(-.05)
+			# 		print(self.talon.getQuadraturePosition())
+			# 	else:
+			# 		self.talon.set(0)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
