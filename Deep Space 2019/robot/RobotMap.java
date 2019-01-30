4fdf9133e064dbd42f7ef1a5d58a1b94cc2ae9e6
dackbusch <47158989+dackbusch@users.noreply.github.com>
2019-01-29 16:16:01 -0800

    Update RobotMap.java

--- Deep Space 2019/robot/RobotMap.java
+++ Deep Space 2019/robot/RobotMap.java
@@ -41,10 +41,12 @@ public class RobotMap {
   public static final int REAR_LEFT_STEER_TalonSRX_CAN_ID   = 13;
   public static final int REAR_RIGHT_STEER_TalonSRX_CAN_ID  = 14;
 
+  public static final int TalonSRX_TIMEOUT = 1000; //units in msec
+
   // Gear Ratio Constants.  All Gear Ratios expressed in Motor revs TO Wheel Revs (ie M:W).  10:1 = 10 revs motor for 1 rev of the wheel //
-  public static final double FINAL_DRIVE_WHEEL_GEAR_RATIO = 10.0/1.0;       // Motor drives the wheel directly, ratio is based on pully sizes //
-  public static final double STEER_MOTOR_GEAR_REDUCTION_RATIO = 10.0/1.0;   // Motor drives gear reduction unit //
-  public static final double STEER_WHEEL_GEAR_RATIO = 5.0/1.0;              // Output of gear reduction drives the steering pully //
+  public static final int FINAL_DRIVE_WHEEL_GEAR_RATIO = 10/1;       // Motor drives the wheel directly, ratio is based on pully sizes //
+  public static final double STEER_MOTOR_GEAR_REDUCTION_RATIO = 10/1;   // Motor drives gear reduction unit //
+  public static final double STEER_WHEEL_GEAR_RATIO = 5/1;              // Output of gear reduction drives the steering pully //
   public static final double FINAL_STEER_WHEEL_GEAR_RATIO = STEER_MOTOR_GEAR_REDUCTION_RATIO * STEER_WHEEL_GEAR_RATIO;
 
   // Deadband Joystick Constants
