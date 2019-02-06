929a5999933b7ec7bb0af157d43fdb1879c8c4ab
dackbusch <47158989+dackbusch@users.noreply.github.com>
2019-02-06 09:11:59 -0800

    Added Constants

--- Deep Space 2019/robot/RobotMap.java
+++ Deep Space 2019/robot/RobotMap.java
@@ -5,32 +5,18 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+/**
+ * The RobotMap is where most constants are defined for easy reference
+ */
+
 package frc.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
-
-  // Constants for Robot Dimensions //
+  // Constants for Robot Dimensions
   public static final double WHEELBASE_LENGTH = 10.0;
   public static final double WHEELBASE_TRACK_WIDTH = 10.0;
   
-  // Adjust wheel speed with this constant, must be <= 1.0//
-  public static final double DRIVE_WHEEL_UNITS_PER_100MS = 600.;
-
-  // CAN Bus IDs for Drivetrain Talon SRX controllers //
+   // CAN Bus IDs for Drivetrain Talon SRX controllers
   public static final int FRONT_RIGHT_DRIVE_TalonSRX_CAN_ID = 1;
   public static final int FRONT_LEFT_DRIVE_TalonSRX_CAN_ID  = 2;
   public static final int REAR_LEFT_DRIVE_TalonSRX_CAN_ID   = 3;
@@ -43,13 +29,23 @@ public class RobotMap {
 
   public static final int TalonSRX_TIMEOUT = 1000; //units in msec
 
-  // Gear Ratio Constants.  All Gear Ratios expressed in Motor revs TO Wheel Revs (ie M:W).  10:1 = 10 revs motor for 1 rev of the wheel //
-  public static final int FINAL_DRIVE_WHEEL_GEAR_RATIO = 10;       // Motor drives the wheel directly, ratio is based on pully sizes //
-  public static final double STEER_MOTOR_GEAR_REDUCTION_RATIO = 12.;    // Motor drives gear reduction unit //
-  public static final double STEER_WHEEL_GEAR_RATIO = 4.6;              // Output of gear reduction drives the steering pully //
+  // Gear Ratio Constants.  All Gear Ratios expressed in Motor revs TO Wheel Revs (ie M:W).  10:1 = 10 revs motor for 1 rev of the wheel
+  public static final double FINAL_DRIVE_WHEEL_GEAR_RATIO = 10.;        // Motor drives the wheel directly, ratio is based on pully sizes
+  public static final double STEER_MOTOR_GEAR_REDUCTION_RATIO = 12.;    // Motor drives gear reduction unit
+  public static final double STEER_WHEEL_GEAR_RATIO = 4.6;              // Output of gear reduction drives the steering pully
   public static final double FINAL_STEER_WHEEL_GEAR_RATIO = STEER_MOTOR_GEAR_REDUCTION_RATIO * STEER_WHEEL_GEAR_RATIO;
-  public static final double STEER_MOTOR_PULSES_PER_REVOLUTION = 4096.;
+ 
+  // Encoder Constants
+  public static final double DRIVE_WHEEL_PULSES_PER_100MS = 600.;       // Used for closed loop velocity
+  public static final double STEER_MOTOR_PULSES_PER_REVOLUTION = 4096.; // Used for closed loop position
+  public static final double STEER_PPR = STEER_MOTOR_PULSES_PER_REVOLUTION * FINAL_STEER_WHEEL_GEAR_RATIO;
 
+  // Locations of the swerve drive index signal for each modle in encoder pulses to get to zero, straight forward
+  public static final double FRONT_RIGHT_STEER_INDEX_OFFSET_PULSES = -STEER_PPR / 4.;
+  public static final double FRONT_LEFT_STEER_INDEX_OFFSET_PULSES = -STEER_PPR  / 4.;
+  public static final double REAR_LEFT_STEER_INDEX_OFFSET_PULSES  = STEER_PPR  / 4.;
+  public static final double REAR_RIGHT_STEER_INDEX_OFFSET_PULSES = STEER_PPR  * 3. / 4.;
+  
   // Deadband Joystick Constants
   public static final double X_AXIS_THREASHOLD = 0.1;
   public static final double Y_AXIS_THREASHOLD = 0.1;
