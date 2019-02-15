221e555e351a61b4f284b77c21b90b8949008860
dackbusch <47158989+dackbusch@users.noreply.github.com>
2019-02-15 08:02:50 -0800

    Add files via upload

--- Deep Space 2019/robot/RobotMap.java
+++ Deep Space 2019/robot/RobotMap.java
@@ -44,21 +44,24 @@ public class RobotMap {
   public static final boolean CHECK_PHASE_DURING_DRIVETRAIN_CALIBRATION = false;
 
   public static final boolean FRONT_RIGHT_DRIVE_TalonSRX_ENCODER_PHASE = true;
-  public static final boolean FRONT_LEFT_DRIVE_TalonSRX_ENCODER_PHASE = false;
-  public static final boolean REAR_LEFT_DRIVE_TalonSRX_ENCODER_PHASE = false;
-  public static final boolean REAR_RIGHT_DRIVE_TalonSRX_ENCODER_PHASE = false;
+  public static final boolean FRONT_LEFT_DRIVE_TalonSRX_ENCODER_PHASE = true;
+  public static final boolean REAR_LEFT_DRIVE_TalonSRX_ENCODER_PHASE = true;
+  public static final boolean REAR_RIGHT_DRIVE_TalonSRX_ENCODER_PHASE = true;
 
   public static final boolean FRONT_RIGHT_STEER_TalonSRX_ENCODER_PHASE = false;
   public static final boolean FRONT_LEFT_STEER_TalonSRX_ENCODER_PHASE = false;
   public static final boolean REAR_LEFT_STEER_TalonSRX_ENCODER_PHASE = false;
   public static final boolean REAR_RIGHT_STEER_TalonSRX_ENCODER_PHASE = false;
 
-  // Gear Ratio Constants.  All Gear Ratios expressed in Motor revs TO Wheel Revs (ie M:W).  10:1 = 10 revs motor for 1 rev of the wheel
+  //
+
+  // Drivetrain Gear Ratio Constants
   public static final double FINAL_DRIVE_WHEEL_GEAR_RATIO = 10.;        // Motor drives the wheel directly, ratio is based on pully sizes
   public static final double STEER_MOTOR_GEAR_REDUCTION_RATIO = 12.;    // Motor drives gear reduction unit
-  public static final double STEER_WHEEL_GEAR_RATIO = 4.6;              // Output of gear reduction drives the steering pully
+  public static final double STEER_WHEEL_GEAR_RATIO = 4.612;            // Output of gear reduction drives the steering pully
   public static final double FINAL_STEER_WHEEL_GEAR_RATIO = STEER_MOTOR_GEAR_REDUCTION_RATIO * STEER_WHEEL_GEAR_RATIO;
  
+  // 
   // Encoder Constants
   public static final double DRIVE_WHEEL_PULSES_PER_100MS = 600.;       // Used for closed loop velocity
   public static final double STEER_MOTOR_PULSES_PER_REVOLUTION = 4096.; // Used for closed loop position
@@ -87,9 +90,9 @@ public class RobotMap {
   public static final double ROLL_THRESHOLD = 20.; //Pitch in degrees
   
   // Vision System Constants
-  public static final double CAMERA_MOUNTING_ANGLE = 30;  //Units are in degrees and referenced to X axis, with CCW being positive
-  public static final double CAMERA_MOUNTING_HEIGHT = 36;  //Units are in inches
-  public static final double DOUBLE_STRIPE_REFLECTIVE_TAPE_TARGET_HEIGHT = 25;  //Units are in inches
+  public static final double CAMERA_MOUNTING_ANGLE = 30.;  //Units are in degrees and referenced to X axis, with CCW being positive
+  public static final double CAMERA_MOUNTING_HEIGHT = 36.;  //Units are in inches
+  public static final double DOUBLE_STRIPE_REFLECTIVE_TAPE_TARGET_HEIGHT = 25.;  //Units are in inches
 
   public static void init(){
   }
