92d5523ccb378718d8c3c6b00cf864092ee88185
Crista Falk <Crista2019@users.noreply.github.com>
2019-02-17 17:02:25 -0800

    Add files via upload

--- Deep Space 2019/robot/commands/CollisionSensor.java
+++ Deep Space 2019/robot/commands/CollisionSensor.java
@@ -9,34 +9,53 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 
-public class BoomerangOut extends Command {
-  public BoomerangOut() {
-    requires(Robot.m_boomerang);
+public class CollisionSensor extends Command {
+  double prevAccelY = 0.;
+  
+  public CollisionSensor() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.m_drivetrain);
   }
-
+  
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   }
-
+  
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.m_boomerang.deployBoomerang();
-  }
+    boolean collisionDetected = false;
+    
+    while (!collisionDetected) {
+      double currentAccelY = Robot.m_gyro.getAccelY();
+      
+      double currentJerkY = currentAccelY - prevAccelY;
+      prevAccelY = currentAccelY;
+      
+      if ( Math.abs(currentJerkY) > RobotMap.COLLISION_THRESHOLD_Y) {
+        collisionDetected = true;
+      }
 
+      Robot.m_drivetrain.move(.5, 0., 0.);
+    } 
+
+    Robot.m_drivetrain.move(0., 0., 0.);
+  }
+  
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return true;
   }
-
+  
   // Called once after isFinished returns true
   @Override
   protected void end() {
   }
-
+  
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
