c5fd0e28f2bacddf6709a198ae1fae1dd42a38b7
dackbusch <47158989+dackbusch@users.noreply.github.com>
2020-01-11 09:14:06 -0800

    Add files via upload

---
+++ Infinite Recharge 2020/robot/subsystems/Drivetrain.java
@@ -0,0 +1,280 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+/* Robot Drivetrain Assignment
+*                        
+*                              Front/Forward       
+*                                   ^             
+*                                   |             
+*                                   |             
+*  front left Drive and Steer |-----------| front right Drive and Steer    
+*                             |           |       
+*                             |           |  l    
+*                             |           |  e    
+*                             |           |  n    
+*                             |           |  g    
+*                             |           |  t    
+*                             |           |  h    
+*                             |           |       
+*  rear left Drive and Steer  |-----------| rear right Drive and Steer 
+*                                 width           
+*
+***********************
+* This Class Builds the Drivetrain
+* It instantiates the Talon Motor Controllers for the drivetrain.
+* It then creates a swerve module for each wheel passing the respective Motor Controllers.
+* It then creates a swere drive object and passes the 4 swerve modules to it.
+* It has simple methods to call for move, stop, brake, coast, toggle Cetnric.
+* The Drive Command depends on it, and Drive is the default command for this
+* subsystem.
+*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.swerve.SwerveModule;
+import frc.robot.swerve.SwerveDrive;
+import java.util.Queue;
+import java.util.LinkedList;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class Drivetrain extends Subsystem {
+  // Create the Drive Motor and Steer Motor Objects
+  private final WPI_TalonSRX frontRightDriveMotor = new WPI_TalonSRX(RobotMap.FRONT_RIGHT_DRIVE_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX frontLeftDriveMotor = new WPI_TalonSRX(RobotMap.FRONT_LEFT_DRIVE_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX rearLeftDriveMotor = new WPI_TalonSRX(RobotMap.REAR_LEFT_DRIVE_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX rearRightDriveMotor = new WPI_TalonSRX(RobotMap.REAR_RIGHT_DRIVE_TalonSRX_CAN_ID);
+  
+  private final WPI_TalonSRX frontRightSteerMotor = new WPI_TalonSRX(RobotMap.FRONT_RIGHT_STEER_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX frontLeftSteerMotor = new WPI_TalonSRX(RobotMap.FRONT_LEFT_STEER_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX rearLeftSteerMotor = new WPI_TalonSRX(RobotMap.REAR_LEFT_STEER_TalonSRX_CAN_ID);
+  private final WPI_TalonSRX rearRightSteerMotor = new WPI_TalonSRX(RobotMap.REAR_RIGHT_STEER_TalonSRX_CAN_ID);
+  
+  private SwerveModule frontRightWheel;
+  private SwerveModule frontLeftWheel;
+  private SwerveModule rearLeftWheel;
+  private SwerveModule rearRightWheel;
+  
+  private SwerveDrive m_SwerveDrive;
+  
+  private boolean centric = false;
+  
+  private double yaw = 0.; //NavX Gyro Yaw angle
+  private double roll = 0.; //NavX Gyro Roll angle
+  private double pitch = 0.; //NavX Gyro Pitch angle
+  private double maxRoll = RobotMap.PITCH_THRESHOLD;
+  private double maxPitch = RobotMap.ROLL_THRESHOLD;
+  private double power = 0;
+  
+  private boolean reverseEn = true;  //Enables reversing wheel drive motors
+  
+  private boolean snakeMode = false; //Crab = false, Snake = True
+
+  private boolean hiLo = false; //High or Low Speed Drivetrain Mode
+
+  private boolean flip180 = false; //Flip a 180, induces rotate until 180 degrees of rotation have been attained
+  private boolean flipCW = false; //Clock Wise = True
+  
+  private boolean ballTrackMode = false;
+  private boolean hatchTrackMode = false;
+
+  private Queue<Double> txQueue = new LinkedList<Double>();
+  private double queueSum = 0.;
+  private double txAverage = 0.;
+  private double tx = 0.;
+  
+  public Drivetrain(){
+    //Create the Swerve Drive Modules for each wheel
+    frontRightWheel = new SwerveModule("FrontRightWheel", frontRightDriveMotor, frontRightSteerMotor);
+    frontLeftWheel = new SwerveModule("FrontLeftWheel", frontLeftDriveMotor, frontLeftSteerMotor);
+    rearLeftWheel = new SwerveModule("RearLeftWheel", rearLeftDriveMotor, rearLeftSteerMotor);
+    rearRightWheel = new SwerveModule("RearRightWheel", rearRightDriveMotor, rearRightSteerMotor);
+    
+    //Now Build the complete Swerve Drive Object with all four Wheel Modules
+    m_SwerveDrive = new SwerveDrive(frontRightWheel, frontLeftWheel, rearLeftWheel, rearRightWheel);
+  }
+  
+  public void init(){
+    System.out.println("**Initializing Drivetrain Motors");
+    m_SwerveDrive.initMotors();
+  }
+  
+  public void reset(){
+    turnOffCentric();
+    setCrabMode();
+    setLowSpeedDriveMode();
+    System.out.println("**Drivetrain reset to CrabMode, Centric Off, Coast, and Low Speed");
+    //m_SwerveDrive.reset(); DO NOT USE UNLESS TESTING WITH KNOWLEDGE OF IMPACT
+  }
+  
+  public void calSteering(boolean checkPhase){
+    m_SwerveDrive.calSteerMotors(checkPhase);
+  }
+  
+  public void turnOnCentric(){
+    centric = true;
+    System.out.println("**Centric set to: "+centric);
+  }
+  
+  public void turnOffCentric(){
+    centric = false;
+    System.out.println("**Centric set to: "+centric);
+  }
+  
+  public boolean getCentric(){
+    return centric;
+  }
+  
+  public void setSnakeMode(){
+    snakeMode = true;
+    System.out.println("**Drivetrain Set To Snake Mode");
+  }
+  
+  public void setCrabMode(){
+    snakeMode = false;
+    System.out.println("**Drivetrain Set To Crab Mode");
+  }
+  
+  public void setHighSpeedDriveMode(){
+    hiLo = true;
+  }
+
+  public void setLowSpeedDriveMode(){
+    hiLo = false;
+  }
+  
+  public void setBallTrackMode(boolean mode){
+    ballTrackMode = mode;
+    if (!ballTrackMode) {
+      txQueue.clear();
+      queueSum = 0.;
+    }
+  }
+
+  public void setHatchTrackMode(boolean mode){
+    hatchTrackMode = mode;
+    if (!hatchTrackMode) {
+      txQueue.clear();
+      queueSum = 0.;
+    }
+  }
+
+  public void setFlip180(boolean directionCW){
+    flip180 = true;
+    flipCW = directionCW;
+  }
+
+  public void move(double fwd, double str, double rcw){
+    // This is Yaw angle +/- 180 in degrees
+    yaw = Robot.m_gyro.getYawDeg();
+    roll = Robot.m_gyro.getRollDeg();
+    pitch = Robot.m_gyro.getPitchDeg();
+    
+    // Detect too much roll angle and strafe into the roll or too much pitch and drive Fwd/Rev accordingly
+    if (roll > maxRoll || roll < -maxRoll) antiRoll(roll);
+    if (pitch > maxPitch || pitch < -maxPitch) antiFlip(pitch);
+    if (flip180) rotate180(fwd, str);
+    
+    // Override Joystick Inputs for str and rcw when using Vision Tracking
+    if (ballTrackMode || hatchTrackMode){
+      tx = Robot.m_vision.getTx();
+
+      // take in 10 pitch readings and average them out
+      if (txQueue.size() < 10){
+        txQueue.add(tx);
+        queueSum += tx;
+      } else { 
+        queueSum-=txQueue.remove();
+        txQueue.add(tx);
+        queueSum += tx;
+      }
+  
+      txAverage = queueSum / txQueue.size();
+      // System.out.println(txAverage);
+
+      if (ballTrackMode) str = 0.;
+
+      rcw = .05*txAverage;
+    }
+    
+    m_SwerveDrive.setMotors(fwd, str, rcw, centric, yaw, reverseEn, snakeMode, hiLo);
+  }
+  
+  private void antiRoll(double roll){
+    setCrabMode();
+    
+    while (roll > 1. || roll < -1.){
+      power = -roll/maxRoll;
+      if (power > 1.) power = 1; else if (power < -1.) power = -1.;
+      if (roll > 1.) m_SwerveDrive.setMotors(0, power, 0., centric, yaw, reverseEn, snakeMode, hiLo);
+      if (roll < -1.) m_SwerveDrive.setMotors(0, power, 0., centric, yaw, reverseEn, snakeMode, hiLo);
+      roll = Robot.m_gyro.getRollDeg();
+    }
+  }
+  
+  private void antiFlip(double pitch){
+    
+    while (pitch > 1. || pitch < -1.){
+      power = -pitch/maxPitch;
+      if (power > 1.) power = 1; else if (power < -1.) power = -1.;
+      if (pitch > 1.) m_SwerveDrive.setMotors(power, 0., 0., centric, yaw, reverseEn, snakeMode, hiLo);
+      if (pitch < -1.) m_SwerveDrive.setMotors(power, 0., 0., centric, yaw, reverseEn, snakeMode, hiLo);
+      pitch = Robot.m_gyro.getPitchDeg();
+    }
+  }
+
+  private void rotate180(double fwdStart, double strStart){
+    //Get the current yaw and initialize local variables
+    double yawCurrent = Robot.m_gyro.getYawAccumDeg(); //Yaw with no discontinuity at +/- 180
+    double yawStart = yawCurrent;
+    double yawDiff = 0.;
+    double rcwMod = 0.;
+    double yawTarg = 0.;
+    double time = Timer.getFPGATimestamp();
+
+    //Check for clockwise or counter clockwise rotation
+    if (flipCW) rcwMod = 1.; else rcwMod = -1.;
+    if (hiLo) yawTarg = 130.; else yawTarg = 160.;
+    //Now rotate 180 degrees either CW or CCW maintaining fwd and str settings
+    while (yawDiff < yawTarg){
+      yawCurrent = Robot.m_gyro.getYawAccumDeg();
+      
+      if (Timer.getFPGATimestamp() - time > 3) break;
+      yawDiff = Math.abs(yawStart - yawCurrent);
+      
+      m_SwerveDrive.setMotors(fwdStart, strStart, rcwMod, centric,  Robot.m_gyro.getYawDeg(), reverseEn, snakeMode, hiLo);
+    }
+
+    //Disable flip so it only flips one time
+    flip180 = false;
+  }
+  
+  public void quickStop(){
+    m_SwerveDrive.stopDriveMotors();
+  }
+  
+  public void emergencyStop(){
+    m_SwerveDrive.emergencyStopMotors();
+  }
+  
+  public void driveDistance(double gyro, boolean hiLo, double distance) {
+    m_SwerveDrive.setMotorsForDistance(0.5, centric, gyro, reverseEn, snakeMode, hiLo, distance);
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    // setDefaultCommand(new Drive());
+  }
+}
---
+++ Infinite Recharge 2020/robot/swerve/SwerveMath.java
@@ -0,0 +1,412 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+/* Robot Wheel Assignment 
+*                        
+*    Front/Forward       
+*          ^             
+*          |             
+*          |             
+*  W2|-----------|W1     
+*    |           |       
+*    |           |  l    
+*    |           |  e    
+*    |           |  n    
+*    |           |  g    
+*    |           |  t    
+*    |           |  h    
+*    |           |       
+*  W3|-----------|W4     
+*        width           
+*
+***********************
+*  This Class takes the desired forward, strafe, and rotate clockwise inputs and 
+*  calculates the wheel speed and wheel angle for each wheel to acheive the desired
+*  motion.  It also provides for gyro correction of the joystick inputs for 
+*  field centric drive mode.
+*/
+
+package frc.robot.swerve;
+
+import frc.robot.RobotMap;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SwerveMath {
+
+  // Fields of SwerveMath, note they are local (ie private to SwerveMath)
+  // These are intitialized as constants to reduce the math cycles in the class' methods
+  private static Double halfLength = RobotMap.WHEELBASE_LENGTH / 2; //length of chassis divided by 2
+  private static Double halfWidth = RobotMap.WHEELBASE_TRACK_WIDTH / 2; //width of chassis divided by 2
+  private static Double radius = Math.sqrt((halfLength * halfLength) + (halfWidth * halfWidth));  //radius of rotational center to each wheel
+  private static Double halfLengthNorm = halfLength / radius;  //Length normalized by radius
+  private static Double halfWidthNorm = halfWidth / radius;  //Width normalized by radius
+
+  private static Double pp100msec = RobotMap.DRIVE_WHEEL_PULSES_PER_100MS; //encoder pulses per 100 msec
+
+  //private static Double toDeg = 180./Math.PI;  //convert Radians to Degrees
+  private static Double toRad = Math.PI/180.;  //convert Degrees to Radians
+
+  // Pulses per various angles for steering the wheel
+  private static Double threeSixty = RobotMap.STEER_PPR;
+  private static Double twoSeventy = threeSixty * .75;
+  private static Double oneEighty = threeSixty / 2.;
+  private static Double ninety = oneEighty / 2.;
+
+  // toPos is used to convert +/-pi angle to encoder position
+  private static Double toPos = oneEighty / Math.PI;
+
+  private static Double fwd; //Forward, Y axis, -1 to 1 from Joystick
+  private static Double str; //Strafe, X axis, 1 to -1 from Joystick
+  private static Double rcw; //Rotate CW, Z axis, 1 to -1 from Joystick, refernced 1=180 CW -1=-180 CW
+
+  private static Double gyro; //Gyro Input in Degrees
+
+  private static boolean centric; //Field Centric Mode
+
+  private static boolean reverseEn; //Reverse Enable Mode
+
+  private static boolean snakeMode; //Crab = false, Snake = true
+
+  private static boolean hiLo; //Adjust wheel velocity scalar for high speed or low speed
+
+  //Initialize variables to store previous SwerveMath iteration's wheel position values - used for Reversing
+  private static Double wp1Current = 0.;  
+  private static Double wp2Current = 0.;  
+  private static Double wp3Current = 0.;  
+  private static Double wp4Current = 0.;  
+
+  //Initialize variables to track whether wheel direction was reversed in previous SwerveMath iteration - used for Reversing
+  private static boolean wp1IsReversed = false;  
+  private static boolean wp2IsReversed = false;  
+  private static boolean wp3IsReversed = false;  
+  private static boolean wp4IsReversed = false;  
+
+  //Initialize variables to track full wheel direction rotations
+  private static Double wp1Rotate = 0.;  
+  private static Double wp2Rotate = 0.;  
+  private static Double wp3Rotate = 0.;  
+  private static Double wp4Rotate = 0.;  
+  
+  // Chose ArrayList type and defined the size because the size will never change and it's easy to modify members 
+  private ArrayList<Double> wheelVectors = new ArrayList<Double>(Arrays.asList(0.,0.,0.,0.,0.,0.,0.,0.));
+  
+  // Null Constructor for SwerveMath for Object Instanitation
+  public SwerveMath(){
+  }
+
+  //Only for testing, don't use this method
+  public void reset(){
+    wp1Current = 0.;  
+    wp2Current = 0.;  
+    wp3Current = 0.;  
+    wp4Current = 0.;  
+
+    wp1IsReversed = false;  
+    wp2IsReversed = false;  
+    wp3IsReversed = false;  
+    wp4IsReversed = false;  
+
+    wp1Rotate = 0.;  
+    wp2Rotate = 0.;  
+    wp3Rotate = 0.;  
+    wp4Rotate = 0.;  
+    System.out.println("  --Swerve Math Reset");
+  }  
+
+  // First Method of SwerveMath that returns the vector (speed, position) for each wheel as a list
+  public ArrayList<Double> getVectors(double fwdIn, double strIn, double rcwIn, boolean centricIn, double gyroIn, boolean reverseEnIn, boolean snakeModeIn, boolean hiLoIn){
+    hiLo = hiLoIn;
+    fwd = fwdIn;
+    str = strIn;
+    if(hiLo) rcw = rcwIn * RobotMap.ROTATE_SCALE; else rcw = rcwIn;
+    centric = centricIn;
+    gyro = gyroIn * toRad;
+    reverseEn = reverseEnIn;
+    snakeMode = snakeModeIn;
+
+    // ********Modify the Joystick Inputs for Snake Mode*********
+    if (snakeMode) {
+      rcw = str;
+      str = 0.;
+    }
+    
+    // ********Modify the Joystick Inputs for Centric Mode*******
+    if (centric) {
+      Double y_f = fwd * Math.cos(gyro); // y component of field
+      Double y_s = str * Math.sin(gyro); // y component of strafe
+      Double x_f = fwd * Math.sin(gyro); // x component of field
+      Double x_s = str * Math.cos(gyro); // x component of strafe
+
+      fwd = y_f + y_s;
+      str = -x_f + x_s;
+    }
+
+    // ********Fundamental Math Block for Wheel Speed and Position*******
+    // Define the common elements in wheel vector math
+    Double A = str - rcw * halfLengthNorm; 
+    Double B = str + rcw * halfLengthNorm;
+    Double C = fwd - rcw * halfWidthNorm;
+    Double D = fwd + rcw * halfWidthNorm;
+
+    // Calculate the speed for each wheel, result will be proportional, no units
+    Double ws1 = Math.sqrt(B*B + C*C);  // Wheel Speed 1 = front right
+    Double ws2 = Math.sqrt(B*B + D*D);  // Wheel Speed 2 = front left
+    Double ws3 = Math.sqrt(A*A + D*D);  // Wheel Speed 3 = rear left
+    Double ws4 = Math.sqrt(A*A + C*C);  // Wheel Speed 4 = rear right
+    
+    // Wheel Speed Normalization, Can't have a wheel speed > 1
+    Double max = ws1;
+    if (ws2 > max) max = ws2;
+    if (ws3 > max) max = ws3;
+    if (ws4 > max) max = ws4;
+    if (max > 1) {
+      ws1 /= max;
+      ws2 /= max;
+      ws3 /= max;
+      ws4 /= max;
+    }
+
+    // modify wheel speeds from <=1 ---> pulses per 100 ms
+    ws1 *= pp100msec; 
+    ws2 *= pp100msec;
+    ws3 *= pp100msec;
+    ws4 *= pp100msec;
+
+    // modify wheel speeds to scale value <=1
+    if (hiLo) {
+      ws1 *= RobotMap.HIGH_SPEED_SCALE; 
+      ws2 *= RobotMap.HIGH_SPEED_SCALE;
+      ws3 *= RobotMap.HIGH_SPEED_SCALE;
+      ws4 *= RobotMap.HIGH_SPEED_SCALE;
+    }
+    else {
+      ws1 *= RobotMap.LOW_SPEED_SCALE; 
+      ws2 *= RobotMap.LOW_SPEED_SCALE;
+      ws3 *= RobotMap.LOW_SPEED_SCALE;
+      ws4 *= RobotMap.LOW_SPEED_SCALE; 
+    }
+
+    // Calculate the wheel angle for each wheel in radians: +/-pi referenced to Y axis 
+    // Then convert to wheel position, with toPos, to absolute pulses for encoder over +/-pi range
+    Double wp1 = Math.atan2(B, C) * toPos; // Wheel Angle 1 = front right
+    Double wp2 = Math.atan2(B, D) * toPos; // Wheel Angle 2 = front left
+    Double wp3 = Math.atan2(A, D) * toPos; // Wheel Angle 3 = rear left
+    Double wp4 = Math.atan2(A, C) * toPos; // Wheel Angle 4 = rear right
+
+    // ********Continous Steering Rotation and Shortest Path Steering with Reversing************
+    // With the output of the math block above there is a discontinuity from 180 degrees to 181 degrees.
+  
+    // This code block tests each wheel to see if it needs to move > +/- 90 degrees.  If so, it will add/sub
+    // 180 degrees to provide the supplemental angle and reverse the drive wheel.  If the wheel needs to 
+    // traverse the discontinuity at +/- 180 it will also track the 360 degree rotation necessary.  
+    // If the steering motion is greater than +/- 270 degrees then no reversing will make sense and the 
+    // code will add/sub 360 degrees and track the 360 degree rotation through the discontinuity, again
+    // for smooth wheel travel and shortest path steering.
+   
+    // First, if any wheel was reversed by the last SwerveMath iteration then reverse it for this iteration
+    // so the new comparison is done with the supplemental angles. Nested "if" statements calculate 
+    // the supplemental angle.
+    
+    if (reverseEn){
+      if (wp1IsReversed) {
+        ws1 *= -1.;
+        if ( wp1 >= 0) wp1 -= oneEighty; else  wp1 += oneEighty;
+      }
+
+      if (wp2IsReversed) {
+        ws2 *= -1.;
+        if ( wp2 >= 0) wp2 -= oneEighty; else  wp2 += oneEighty;
+      }
+
+      if (wp3IsReversed) {
+        ws3 *= -1.;
+        if ( wp3 >= 0) wp3 -= oneEighty; else  wp3 += oneEighty;
+      }
+
+      if (wp4IsReversed) {
+        ws4 *= -1.;
+        if ( wp4 >= 0) wp4 -= oneEighty; else  wp4 += oneEighty;
+      }
+    }
+
+    // Now look to see if the wheel needs to move more than 90 degrees or more than 270 degrees
+    // Do this for each wheel position
+    switch (pulseDiff(wp1, wp1Current, wp1Rotate)){
+      case "< -90":  //If true then reverse the wheel
+        ws1 *= -1.;
+        wp1IsReversed = !wp1IsReversed;
+        if (wp1 >= 0) {
+          wp1 -= oneEighty;
+          wp1Rotate += threeSixty;  //Rotate throught the discontinuity at +/- 180
+        } 
+        else {
+          wp1 += oneEighty; 
+        }
+      break;
+
+      case "> 90":  //If true then reverse the wheel
+        ws1 *= -1.;
+        wp1IsReversed = !wp1IsReversed;
+        if (wp1 >= 0) {
+          wp1 -= oneEighty;
+        } 
+        else {
+          wp1 += oneEighty; 
+          wp1Rotate -= threeSixty;  //Rotate throught the discontinuity at +/- 180
+        }
+      break;
+      
+      case "< -270":  //If true rotate through the discontinuity at +/- 180
+        wp1Rotate += threeSixty; 
+      break;
+      
+      case "> 270":  //If true rotate through the discontinuity at +/- 180
+        wp1Rotate -= threeSixty;
+      break;
+    }
+
+    switch (pulseDiff(wp2, wp2Current, wp2Rotate)){
+      case "< -90":  
+        ws2 *= -1.;
+        wp2IsReversed = !wp2IsReversed;
+        if (wp2 >= 0) {
+          wp2 -= oneEighty;
+          wp2Rotate += threeSixty; 
+        } 
+        else {
+          wp2 += oneEighty; 
+        }
+      break;
+
+      case "> 90": 
+        ws2 *= -1.;
+        wp2IsReversed = !wp2IsReversed;
+        if (wp2 >= 0) {
+          wp2 -= oneEighty;
+        } 
+        else {
+          wp2 += oneEighty; 
+          wp2Rotate -= threeSixty;  
+        }
+      break;
+
+      case "< -270":
+        wp2Rotate += threeSixty; 
+      break;
+
+      case "> 270":
+        wp2Rotate -= threeSixty;
+      break;
+    }
+
+    switch (pulseDiff(wp3, wp3Current, wp3Rotate)){
+      case "< -90": 
+        ws3 *= -1.;
+        wp3IsReversed = !wp3IsReversed;
+        if (wp3 >= 0) {
+          wp3 -= oneEighty;
+          wp3Rotate += threeSixty;  
+        } 
+        else {
+          wp3 += oneEighty; 
+        }
+      break;
+
+      case "> 90":  
+        ws3 *= -1.;
+        wp3IsReversed = !wp3IsReversed;
+        if (wp3 >= 0) {
+          wp3 -= oneEighty;
+        } 
+        else {
+          wp3 += oneEighty; 
+          wp3Rotate -= threeSixty;  
+        }
+      break;
+
+      case "< -270":
+        wp3Rotate += threeSixty; 
+      break;
+
+      case "> 270":
+        wp3Rotate -= threeSixty;
+      break;
+    }
+
+    switch (pulseDiff(wp4, wp4Current, wp4Rotate)){
+      case "< -90":  
+        ws4 *= -1.;
+        wp4IsReversed = !wp4IsReversed;
+        if (wp4 >= 0) {
+          wp4 -= oneEighty;
+          wp4Rotate += threeSixty;  
+        } 
+        else {
+          wp4 += oneEighty; 
+        }
+      break;
+
+      case "> 90":
+        ws4 *= -1.;
+        wp4IsReversed = !wp4IsReversed;
+        if (wp4 >= 0) {
+          wp4 -= oneEighty;
+        } 
+        else {
+          wp4 += oneEighty; 
+          wp4Rotate -= threeSixty;
+        }
+      break;
+
+      case "< -270":
+        wp4Rotate += threeSixty; 
+      break;
+
+      case "> 270":
+        wp4Rotate -= threeSixty;
+      break;
+    }
+
+    // Add the rotation pulse accumulator to the wheel position to normalize to the encoder position
+    wp1 += wp1Rotate;
+    wp2 += wp2Rotate;
+    wp3 += wp3Rotate;
+    wp4 += wp4Rotate;
+ 
+    //Finally, save modified "wp" position from this SwerveMath iteration to be used in next iteration//
+    wp1Current = wp1;
+    wp2Current = wp2;
+    wp3Current = wp3;
+    wp4Current = wp4;
+
+    // Set the vector values (speed, position) for each wheel vector
+    wheelVectors.set(0,ws1);
+    wheelVectors.set(1,wp1);
+    wheelVectors.set(2,ws2);
+    wheelVectors.set(3,wp2);
+    wheelVectors.set(4,ws3);
+    wheelVectors.set(5,wp3);
+    wheelVectors.set(6,ws4);
+    wheelVectors.set(7,wp4);
+
+    return wheelVectors;
+  }
+
+  // This is a private method to simplify angle finding and make the code more readable
+  private String pulseDiff(Double wp, Double wpCurrent, Double wpRotate){
+    Double diff = wp - (wpCurrent - wpRotate); //Find the rotation requested, backing out the rotate pulse accumulator
+    String angle = "";
+
+    if (diff < -twoSeventy) angle = "< -270"; else if ((diff < -ninety) && reverseEn) angle = "< -90";
+    if (diff > twoSeventy) angle = "> 270"; else if ((diff > ninety) && reverseEn) angle = "> 90";
+
+    return angle;
+  }
+}
+
